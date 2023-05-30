6773afe60ad49e3827002b35ac797a5295a9af52
SuganthanHarreshnan <134834158+SuganthanHarreshnan@users.noreply.github.com>
2023-05-30 00:59:44 -0700

    Add files via upload

---
+++ My First program.py
@@ -0,0 +1,21 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:/python/My First program.py ====================
+Hello,World!
+what is your name?Suganthan Harreshnan
+Hello Suganthan Harreshnan
+>>> 
+>>> age=13
+>>> print(age)
+13
+>>> 
+>>> Weight=43
+>>> print(Weight)
+43
+>>> 
+>>> Height=163
+>>> print(Height)
+163
+>>> 
+>>> 
